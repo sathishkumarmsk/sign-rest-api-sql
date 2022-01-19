f272853451e70c2776aa375f5f4597596faacac6
sathishkumarmsk <sathishkumarmsk5699@gmail.com>
2022-01-19 12:31:19 +0530

    basic express api

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("msk")
+import express from 'express';
+
+const app = express();
+const port = 8000;
+
+app.use('/', (req, res) => res.send('Hello Chaal'));
+
+app.listen(port, () => console.log(`Running on http://localhost:${port}`)); 
